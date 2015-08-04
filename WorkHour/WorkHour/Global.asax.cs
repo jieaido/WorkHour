674f4017a1cba6dbe16308b1a33339f52396b4d6
jieaido <jieaido@163.com>
2015-08-04 23:36:17 +0800

    框架开始

--- WorkHour/WorkHour/Global.asax.cs
+++ WorkHour/WorkHour/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WorkHour.Models;
 
 namespace WorkHour
 {
@@ -12,6 +14,7 @@ namespace WorkHour
     {
         protected void Application_Start()
         {
+            System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<WHDB>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
